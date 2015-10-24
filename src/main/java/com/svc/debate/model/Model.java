892c45d6f80337f8ca841dc2aae0d201a80036c1
Dhawal Sawla <idhavalsavla@gmail.com>
2015-10-24 13:51:12 -0700

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	src/main/java/com/svc/debate/Main.java

---
+++ src/main/java/com/svc/debate/model/Model.java
@@ -0,0 +1,14 @@
+/*
+package com.svc.debate.model;
+import java.util.*;
+
+*/
+/**
+ * Created by dsawla on 10/24/2015.
+ *//*
+
+public interface Model {
+    public void insertPost();
+    public void getAllPosts();
+    boolean existPost();
+}*/
