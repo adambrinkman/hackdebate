08de32161ab35b9dc7c23f23fcf5bff8b60d4025
SpencerDo <magicdo7@gmail.com>
2015-10-24 04:05:40 -0700

    initial commit

---
+++ src/main/java/com/svc/debate/Main.java
@@ -0,0 +1,10 @@
+package com.svc.debate;
+
+/**
+ * Created by doyonghoon on 2015. 10. 24..
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("hello");
+  }
+}
