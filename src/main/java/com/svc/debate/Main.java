e3f2cbc65acd53784d570686e78b7d0d09a5593a
SpencerDo <magicdo7@gmail.com>
2015-10-24 04:19:50 -0700

    constructs basic part of the project.

--- src/main/java/com/svc/debate/Main.java
+++ src/main/java/com/svc/debate/Main.java
@@ -1,10 +1,30 @@
 package com.svc.debate;
 
+import com.svc.debate.service.MainService;
+import freemarker.cache.ClassTemplateLoader;
+import freemarker.template.Configuration;
+import spark.ModelAndView;
+import spark.Spark;
+import spark.template.freemarker.FreeMarkerEngine;
+
+import static spark.Spark.get;
+
 /**
  * Created by doyonghoon on 2015. 10. 24..
  */
 public class Main {
   public static void main(String[] args) {
-    System.out.println("hello");
+    Spark.staticFileLocation("/assets");
+    FreeMarkerEngine freeMarkerEngine = new FreeMarkerEngine();
+    Configuration freeMarkerConfiguration = new Configuration();
+    freeMarkerConfiguration.setTemplateLoader(new ClassTemplateLoader(MainService.class, "/"));
+    freeMarkerEngine.setConfiguration(freeMarkerConfiguration);
+
+    Spark.port(Integer.parseInt(System.getenv("PORT")));
+    get("/", (req, res) -> {
+      res.status(200);
+      res.type("text/html");
+      return freeMarkerEngine.render(new ModelAndView(null, "assets/home.ftl"));
+    });
   }
 }
---
+++ src/main/java/com/svc/debate/service/MainService.java
@@ -0,0 +1,7 @@
+package com.svc.debate.service;
+
+/**
+ * Created by doyonghoon on 2015. 10. 24..
+ */
+public class MainService {
+}
